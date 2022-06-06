ceaf099caaa9d193dc85233afa4261508e78f3b8
Xen0o2 <alexlecoutre@gmail.com>
2022-06-06 21:36:48 +0200

    Editing version and typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,18 @@
 declare module "VintedMonitor";
+
+
+export class VintedMonitor {
+    /**
+     * 
+     * @param url The url to monitor
+     */
+    watch(url: string): VintedMonitor;
+
+    /**
+     * 
+     * @param callback The function to run when a new item is detected, with as first argument, the item
+     */
+    onItemFound(callback: CallableFunction): void;
+}
+
+import VintedItemType from "./types/VintedItemType.js";
